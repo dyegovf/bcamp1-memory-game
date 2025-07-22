3f94a9647f698602f615fdd72b878378557eba4d
dyegovf <dyegovf@gmail.com>
2025-07-22 17:10:52 -0300

    refactor: replace createDiv function with createCards and update button functionality

--- js/script.js
+++ js/script.js
@@ -1,28 +1,20 @@
-function createDiv() {
-  var div = document.createElement("screen");
-  var imgDiv = document.createTextNode("img");
-  div.setAttribute("style", "background-color: red;");
-  div.appendChild(imgDiv);
-  document.body.appendChild(div);
+function createCards() {
+  const screen = document.getElementById("screen");
+  const colors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "purple",
+    "orange",
+    "pink",
+    "cyan",
+  ];
+
+  colors.forEach((color) => {
+    const card = document.createElement("div");
+    card.className = "box";
+    card.style.backgroundColor = color;
+    screen.appendChild(card);
+  });
 }
-
-// function myFunction() {
-//   var x = document.createElement("DIV");
-//   var t = document.createTextNode("This is a div element.");
-//   x.setAttribute("style", "background-color: pink;");
-//   x.appendChild(t);
-//   document.body.appendChild(x);
-// }
-
-// let screen = document.getElementById("screen");
-
-// let colors = [
-//   "red",
-//   "blue",
-//   "green",
-//   "yellow",
-//   "purple",
-//   "orange",
-//   "pink",
-//   "cyan",
-// ];
